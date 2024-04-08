ddab88f94081e12e6398219e90b17892652ac36b
Aligern <sasso.luca93@gmail.com>
2024-04-08 15:44:42 +0200

    add button function

--- Js/script.js
+++ Js/script.js
@@ -6,27 +6,28 @@ createApp({
             emails: [],
             numEmails: 10,
             api_path:'https://flynn.boolean.careers/exercises/api/random/mail',
-
         }
     },
     methods: {
-        // here we  have our function that will generate the emails
+        // here we  have our function that will generate the emails //
         getRandomEmails() {
+            this.emails = [];
             for (let i = 0; i < this.numEmails; i++) {
-                // we make the API do the work
+                // we make the API do the work //
                 axios.get(this.api_path).then((res)=>{
-                    console.log(res.data);
+                   // console.log(res.data);
                     const email = res.data.response
                     this.emails.push(email);
-                })
+                }) ;
+              // console.log(this.emails);
             }
         }
     },
     created() {
-
+        this.getRandomEmails();
     },
     mounted() {
-        // we print directly the list into the DOM
-        this.getRandomEmails();
+        // we print directly the list into the DOM //
+        
     }
 }).mount('#app');
