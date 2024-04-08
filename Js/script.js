b56a4cdbfb48ee582ee4713fda96fc2930eff524
Aligern <sasso.luca93@gmail.com>
2024-04-08 13:50:47 +0200

    add click,add item on li, getRandomEmails work in prog

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,26 @@
 const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            emails: [],
+            numEmails: 10,
+            api_path:'https://flynn.boolean.careers/exercises/api/random/mail',
+
+        }
+    },
+    methods: {
+        // here we  have our function that will generate the emails
+        getRandomEmails() {
+            for (let i = 0; i < this.numEmails; i++) {
+                
+            }
+        }
+    },
+    created() {
+
+    },
+    mouted() {
+
+    }
+}).mount('#app');
